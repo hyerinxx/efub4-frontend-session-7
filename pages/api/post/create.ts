7437bd64d69df82e3246d7827e7eeadcec547866
Hyerin Sung <hyerinx@naver.com>
2024-11-19 00:23:26 +0900

    ♻️ 게시글 작성 API 리팩토링

--- pages/api/post/create.ts
+++ pages/api/post/create.ts
@@ -1,11 +1,18 @@
 import type { NextApiHandler } from "next";
-import type { InsertOneResult } from "mongodb";
-import type Post from "@/models/post";
 import { postCollection } from "@/utils/database";
 
-const handler: NextApiHandler<InsertOneResult<Post>> = async (req, res) => {
-  const result = await postCollection.insertOne(req.body);
-  return res.redirect(302, "/list");
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method === "POST") {
+    if (!req.body.title || !req.body.content) {
+      return res.status(500).json({ error: "빈 형식이 있습니다." });
+    }
+    try {
+      const result = await postCollection.insertOne(req.body);
+      return res.redirect(302, "/list");
+    } catch (error) {
+      console.error(error);
+    }
+  }
 };
 
 export default handler;
