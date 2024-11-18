1025a1efeacb65e6bce5df147d83280751e3d959
Hyerin Sung <hyerinx@naver.com>
2024-11-19 01:10:53 +0900

    🔨 API 연결

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,10 +1,24 @@
 "use client";
 
 import type Post from "@/models/post";
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const List = () => {
   const [posts, setPosts] = useState<Post[]>([]);
+
+  const readPostList = async () => {
+    try {
+      const response = await axios.get("/api/post/readList");
+      setPosts(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    readPostList();
+  }, []);
 
   return (
     <ul>
